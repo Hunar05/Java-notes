9a09ec8f8af5378fecc97526a1175e12491859b2
Hunar05 <hunarverma2005@gmail.com>
2026-05-01 18:22:05 +0530

    Added Introduction

---
+++ Introduction.java
@@ -0,0 +1,61 @@
+/*public class Introduction {
+    public static void main(String[] args) {
+   System.out.println("Hello world");
+  }
+}*/
+
+//JAVE COUSE BY CODE WITH HARRY :- 
+/* 
+(1)java is an object oriented programming language developed by sum microsystem of usa in 1991 
+
+(2)It was originally called oak by James goslin 
+
+(3)JAVA = purely object oriented
+*/
+
+//Q How java works ?
+/* 
+Ans java is compiled into the bytes code and then it is interpreted to machine code  
+*/
+
+// JAVA installation :- 
+
+/*
+(1)Go to google and type "Install JDK" ==> Install java JDK 
+  
+(2) Go to google and type "Install intellij idea" ==> install java IDE
+  
+# JDK :- java development kit = collection of tools used for developing and running java programs 
+
+# JRE :- java runtime environment = Helps in executing program developed in java 
+ */
+
+// Naming conventions :- 
+
+/* 
+(1)For classes we use pascal convention. first and subequent character from a word are capital letter (uppercase  )
+ 
+Ex :- Main, My scanner,My employee,Code with harry
+
+(2)for functions and variable, we use camel case convention. here first character is lowercase and the subsequent character are upper case 
+
+Ex:- main,myScanner,myMarks,code With Harry
+ */
+
+// Anatomy of a java program :- 
+/*
+ * Documentation section ---> suggested 
+  
+ * package statement ---> optional  
+  
+ * Import statement ---> optional  
+  
+ * Interface statement ---> optional  
+  
+ * class defination ---> optional  
+  
+ * main method class  
+ {
+   main method defination ---> essential
+ } 
+*/
